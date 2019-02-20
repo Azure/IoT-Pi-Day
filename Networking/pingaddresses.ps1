81bec3eeda366b90cd01be0c71e548b3981fd4cb
Erick Moore <ermoor@microsoft.com>
2019-02-20 17:57:01 -0500

    uploaded first pass at script to find mac addresses

--- Networking/pingaddresses.ps1
+++ Networking/pingaddresses.ps1
@@ -1,9 +1,20 @@
 $prefix = "192.168.10."
 $addressCount = 254
 $i = 1
+
 do { 
     ping $prefix$i -n 1 -w 2 | Out-Null
     Write-Progress -Activity "Finding devices" -CurrentOperation $prefix$i -PercentComplete (($i / $addressCount) * 100)
     $i ++
     }
 while ($i -le $addressCount)
+
+$inputFile = Import-Csv "piMaclist.csv"
+
+foreach ($device in $inputFile) {
+
+    arp -a | select-string $device.MacAddress |% { $_.ToString().Trim().Split(" ")[0] }
+
+}
+
+
