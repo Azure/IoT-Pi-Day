594d156227e9d2883351635dd393caca3fea5bd5
Erick Moore <ermoor@microsoft.com>
2019-02-21 06:49:35 -0500

    added HTML output

--- Setting up the Raspberry Pi/MSFT Networking/pingaddresses.ps1
+++ Setting up the Raspberry Pi/MSFT Networking/pingaddresses.ps1
@@ -2,6 +2,14 @@ $prefix = "192.168.10." # // Set your local guest wi-fi network IP prefix here
 $addressCount = 254
 $i = 1
 $fileURL = "https://raw.githubusercontent.com/Azure/IoT-Pi-Day/master/Setting%20up%20the%20Raspberry%20Pi/MSFT%20Networking/piMaclist.csv"
+$deviceList = @()
+
+$Header = @"
+<style>
+TABLE {border-width: 2px; border-style: solid; border-color: blue; border-collapse: collapse;}
+TD {border-width: 2px; padding: 2px; text-align: center; border-style: solid; border-color: blue;}
+</style>
+"@
 
 # // Grab master CSV list from GitHub and convert URL object content to CSV
 
@@ -12,9 +20,11 @@ $inputFile = ConvertFrom-Csv -InputObject $inputFile
 # // Ping all network devices to build local arp table
 
 do { 
+
     ping $prefix$i -n 1 -w 2 | Out-Null
     Write-Progress -Activity "Finding devices" -CurrentOperation $prefix$i -PercentComplete (($i / $addressCount) * 100)
     $i ++
+
     }
 while ($i -le $addressCount)
 
@@ -22,7 +32,21 @@ while ($i -le $addressCount)
 
 foreach ($device in $inputFile) {
 
-    $piDevice = arp -a | select-string $device.MacAddress | foreach { $_.ToString().Trim().Split(" ")[0] }
-    Write-Host $device.DeviceName  " "  $piDevice
+    $piIP = arp -a | select-string $device.MacAddress | foreach { $_.ToString().Trim().Split(" ")[0] }
+
+    if ($piIP -eq $null) {$piIP = "Device Not Found"; $connectString = ""} else {$connectString = "ssh pi@$piIP"}
+
+    $deviceObj = New-Object System.Object
+    $deviceObj | Add-Member -NotePropertyName DeviceName -NotePropertyValue $device.DeviceName
+    $deviceObj | Add-Member -NotePropertyName IPAddress -NotePropertyValue $piIP
+    $deviceObj | Add-Member -NotePropertyName ConnectString -NotePropertyValue $connectString
+
+    $deviceList += $deviceObj
+
+    Write-Host $device.DeviceName  " "  $piIP
 
 }
+
+# // Create HTML file with device name and IP address
+
+$deviceList | ConvertTo-Html -Head $Header | Out-File city.html
